d72e66f15c6804b25af9258264f24b377d49da1a
amitnir29 <54716841+amitnir29@users.noreply.github.com>
2020-01-28 08:19:41 +0200

    Manhattan Distance :)

--- ex4/AStarComparator.h
+++ ex4/AStarComparator.h
@@ -5,13 +5,18 @@
 class AStarComparator {
 	//false- means Best, true- is A*
 	//private bool algo= false;
+
+	State<std::pair<int, int>>* goal;
+
 public:
+    AStarComparator(State<std::pair<int, int>> *goal) : goal(goal) {}
+
 	bool operator ()(State<std::pair<int,int>>* lhs, State<std::pair<int, int>>* rhs) {
 		return lhs->getCost() + manhatanDistance(lhs) > rhs->getCost()+ manhatanDistance(rhs);
 	}
 	int manhatanDistance(State<std::pair<int, int>>* state) {
-		//TODO fill it
-		return 2;
+		return (abs(state->getState().first - goal->getState().first) +
+		abs(state->getState().second - goal->getState().second));
 	}
 };
 #endif
